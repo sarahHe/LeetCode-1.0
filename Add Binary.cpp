8acdbf1c82c23a5fec6beda398a2de67d39e756e
sarahHe <hesha@umich.edu>
2015-01-14 23:38:52 -0500

    2015.1.14 Be careful about the details

---
+++ Add Binary.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.length() - 1, j = b.length() - 1;
+        string res = "";
+        int flag = 0;
+        while (i >= 0 && j >= 0) {
+            int k = a[i]-'0' + b[j]-'0';
+            if (k + flag > 1) {
+                res = to_string(k + flag - 2) + res;
+                flag = 1;
+            }
+            else {
+                res = to_string(k + flag) + res;
+                flag = 0;
+            }
+            i--; j--;
+        }
+        if (i < 0) {
+            while (j >= 0) {
+                if (b[j] - '0' + flag > 1) {
+                    res = '0' + res;
+                }
+                else {
+                    int tmp = b[j] - '0' + flag;
+                    res = to_string(tmp) + res;
+                    flag = 0;
+                }
+                j--;
+            }
+            if (flag)
+                res = '1' + res;
+        }
+        else if (j < 0) {
+            while (i >= 0) {
+                if (a[i]-'0' + flag > 1) {
+                    res = '0' + res;
+                }
+                else {
+                    int tmp = a[i] - '0' + flag;
+                    res = to_string(tmp) + res;
+                    flag = 0;
+                }
+                i--;
+            }
+            if (flag)
+                res = '1' + res;
+        }
+        return res;
+    }
+};
