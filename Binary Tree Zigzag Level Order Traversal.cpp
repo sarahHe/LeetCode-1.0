3d9c84e6ddf19ff954c9c57932a753c690b7224a
sarahHe <hesha@umich.edu>
2015-01-16 01:51:49 -0500

    2015.1.15 Should be very clear about the logic!

--- Binary Tree Zigzag Level Order Traversal.cpp
+++ Binary Tree Zigzag Level Order Traversal.cpp
@@ -13,25 +13,41 @@ public:
         vector<vector<int> > res;
         if (root == NULL)   return  res;
         
-        vector<TreeNode*>   level;
+        deque<TreeNode*>   level;
         level.push_back(root);
-        int k = 1;
+        vector<int> value;
+        value.push_back(root->val);
+        res.push_back(value);
         while (!level.empty()) {
-            res.push_back(level);///////////
-            if (level.size() % 2 == 1) {
-                vector<int> value;
-                for (int i = res.back().size() - 1; i >= 0; i--) {
-                    value.push_back(level[i]->right->val);
-                    value.push_back(level[i]->left->val);
+            value.clear();
+            deque<TreeNode*> tmp;
+            while (!level.empty()) {
+                if (res.size() % 2) {
+                    if (level.back()->right) {
+                        tmp.push_back(level.back()->right);
+                        value.push_back(level.back()->right->val);
+                    }
+                    if (level.back()->left) {
+                        tmp.push_back(level.back()->left);
+                        value.push_back(level.back()->left->val);
+                    }
                     
                 }
-            }
-            else {
-                for (int i = 0; i < res.back().size(); i++) {
-                    level.push_back(res.back()[i]);
+                else {
+                    if (level.back()->left) {
+                        tmp.push_back(level.back()->left);
+                        value.push_back(level.back()->left->val);
+                    }
+                    if (level.back()->right) {
+                        tmp.push_back(level.back()->right);
+                        value.push_back(level.back()->right->val);
+                        }
                 }
+                level.pop_back();
             }
-                
+            level = tmp;
+            if (!tmp.empty())
+                res.push_back(value);
         }
         return res;
     }
