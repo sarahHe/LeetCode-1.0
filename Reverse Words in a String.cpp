6111131bee150e5715d5a77650176650b8622703
sarahHe <hesha@umich.edu>
2015-01-17 00:51:56 -0500

    2015.1.16 think about stack when doing reverse

---
+++ Reverse Words in a String.cpp
@@ -0,0 +1,37 @@
+/*
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+之前用的递归的方法 空间复杂度高。
+看到reverse 就要想到 可以用栈
+*/
+class Solution {
+public:
+    void reverseWords(string &s) {
+        if (s == "")    return;
+        stack<string> reverse;
+        int len = s.length();
+        int i = 0;
+        while (i < len) {
+            int k = i;
+            while (s[k] == ' ' && k < len)
+                k++;
+            i = k; //以上三行是为了“ a”这种case。以空格开始。
+            while (s[k] != ' ' && k < len)
+                k++;
+            reverse.push(s.substr(i, k - i));
+            while (s[k] == ' ' && k < len)
+                k++;
+            i = k;
+        }
+        s = "";
+        while (!reverse.empty()) {
+            s += reverse.top() + " ";
+            reverse.pop();
+        }
+        s = s.substr(0, s.length() - 1);
+    }
+};
