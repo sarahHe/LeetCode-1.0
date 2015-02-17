f6d2b35d46b9350f5f185b7b8a51d4029ace391c
sarahHe <hesha@umich.edu>
2015-02-16 20:38:13 -0500

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -28,6 +28,21 @@ public int uniquePaths(int m, int n) {
     return res[n-1];  
 }
 
+2015.2.16
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n+1, 1);
+        dp[0] = 0; 
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[j] = dp[j] + dp[j-1];
+            }
+        }
+        return dp.back();
+    }
+};
+
 方法二：
 class Solution {
 public:
