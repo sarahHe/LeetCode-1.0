3a43daa3061ab384b3def38292ff2b4deb177cd5
sarahHe <hesha@umich.edu>
2015-02-17 10:17:33 -0500

    Update Container With Most Water.cpp

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -9,13 +9,10 @@ Note: You may not slant the container.
 class Solution {
 public:
     int maxArea(vector<int> &height) {
-        int tmp = min(height[0], height.back()) * (height.size()-1), 
-            area = tmp, i = 0, j = height.size() - 1;
+        int i = 0, j = height.size()-1;
+        int area = 0;
         while (i < j) {
-            if (tmp < min(height[i], height[j]) * (j-i))
-                tmp = min(height[i], height[j]) * (j-i);
-            area = max(tmp, area);
-            
+            area = max(area, (j-i)*min(height[i], height[j]));
             if (height[i] < height[j])
                 i++;
             else
