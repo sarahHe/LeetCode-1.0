f3b720557cf83b2a0487a943bb452d8f47f7102a
sarahHe <hesha@umich.edu>
2015-02-18 22:29:57 -0500

    Update Recover Binary Search Tree.cpp

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -6,7 +6,13 @@
 // 有一个技巧是如果遍历整个序列过程中只出现了一次次序错误，
 // 说明就是这两个相邻节点需要被交换。如果出现了两次次序错误，那就需要交换这两个节点。
 
-//this is correct
+// Two elements of a binary search tree (BST) are swapped by mistake.
+// Recover the tree without changing its structure.
+
+// 递归中序遍历二叉树，设置一个pre指针，记录当前节点中序遍历时的前节点，
+// 如果当前节点大于pre节点的值，说明需要调整次序。
+// 有一个技巧是如果遍历整个序列过程中只出现了一次次序错误，
+// 说明就是这两个相邻节点需要被交换。如果出现了两次次序错误，那就需要交换这两个节点。
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -18,24 +24,44 @@
  */
 class Solution {
 public:
-    //copy by reference
-    void find(TreeNode *root, TreeNode *&p, TreeNode *&q, TreeNode *&prev) {
+    void recoverTree(TreeNode *root) {
         if (!root)  return;
         
-        find(root->left, p, q, prev);
-        if (prev && root->val > prev->val) {
-            if (p == NULL) 
-                p = prev;
-            q = root;
+        TreeNode *f1 = NULL, *f2 = NULL,
+            *pre, *cur = root, *parent = NULL;
+        while (cur) {
+            if (cur->left == NULL) {
+                if (parent && parent->val > cur->val) {
+                    if (f1 == NULL)
+                        f1 = parent;
+                    f2 = cur;
+                }
+                parent = cur;
+                cur = cur->right;
+            }
+            else { 
+                pre = cur->left;
+                while (pre->right && pre->right != cur)
+                    pre = pre->right;
+                    
+                if (pre->right == NULL) {
+                    pre->right = cur;
+                    cur = cur->left;
+                }
+                else {
+                    pre->right = NULL;
+                    if (parent->val > cur->val) {
+                        if (f1 == NULL)
+                            f1 = parent;
+                        f2 = cur;
+                    }
+                    parent = cur;
+                    cur = cur->right;
+                }
+            }
         }
-        prev = root;
-        find(root->right, p, q, prev);
-    }
-
-    void recoverTree(TreeNode *root) {
-        TreeNode *p = NULL, *q=NULL, *prev=NULL;
-        find(root, p, q, prev);
-        if (p != NULL && q != NULL) 
-            swap(p->val, q->val);
+        if (f1 && f2)
+            swap(f1->val, f2->val);
     }
 };
+
