4f2600acf736710773438e839194fa3b165c60f1
sarahHe <hesha@umich.edu>
2015-02-24 20:57:44 -0500

    Update Distinct Subsequences.cpp

--- Distinct Subsequences.cpp
+++ Distinct Subsequences.cpp
@@ -7,43 +7,21 @@
 // S = "rabbbit", T = "rabbit"
 // Return 3.
 
+// 此题需要使用大数运算。使用一点 DP 即可。关键是如何得到递推关系，可以这样想，设母串的长度为 j，  
+// 子串的长度为 i，我们要求的就是长度为 i 的字串在长度为 j 的母串中出现的次数，设为 t[i][j]，若母串的最后一个字符与子串的最后一个字符不同，则长度为 i 的子串在长度为 j 的母串中出现的次数就是母串的前 j - 1 个字符中子串出现的次数，即 t[i][j] = t[i][j - 1]，若母串的最后一个字符与子串的最后一个字符相同，那么除了前 j - 1 个字符出现字串的次数外，还要加上子串的前 i - 1 个字符在母串的前 j - 1 个字符中出现的次数，即 t[i][j] = t[i][j - 1] + t[i - 1][j]。  
 class Solution {
 public:
     int numDistinct(string S, string T) {
-        if (S == "" || S.length() < T.length())
+        if (S.length() < T.length())
             return 0;
-        vector<vector<int> > res(S.length() + 1, vector<int> (T.length() + 1, 0));
-        for (int i = 0; i <= S.length(); i++)
-            res[i][0] = 1;
-        
-        for (int i = 1; i <= S.length(); i++) {
-            for (int j = 1; j <= T.length(); j++) {
-                if (S[i-1] == T[j-1])
-                    res[i][j] = res[i-1][j-1] + res[i-1][j];
-                else
-                    res[i][j] =  res[i-1][j];
+        vector<int> dp(T.length()+1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = T.length() - 1; j >= 0; j--) {// j应该从尾到头，因为每次要使用上一次loop的值。如果从头往尾扫的话，重复计算了。
+                if (S[i] == T[j])
+                    dp[j+1] += dp[j]; 
             }
         }
-        return res.back().back();
+        return dp.back();
     }
 };
-
-//another solution using O(m) space
-public int numDistinct(String S, String T) {
-  if(T.length()==0)
-  {
-    return 1;
-  }
-  if(S.length()==0)
-    return 0;
-  int[] res = new int[T.length()+1];
-  res[0] = 1;
-  for(int i=0;i<S.length();i++)
-  {
-    for(int j=T.length()-1;j>=0;j--)
-    {
-      res[j+1] = (S.charAt(i)==T.charAt(j)?res[j]:0)+res[j+1];
-    }
-  }
-  return res[T.length()];
-}
