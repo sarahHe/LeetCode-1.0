1fedd8009fa674f4d47c70494398fe1d945c3b72
sarahHe <hesha@umich.edu>
2015-03-01 17:59:23 -0500

    Update and rename Plus One to Plus One.cpp

--- Plus One.cpp
+++ Plus One.cpp
@@ -28,3 +28,29 @@ public:
         return digits;
     }
 };
+
+
+2015.3.1 update
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int next = 1;
+        for (int i = digits.size()-1; i >= 0; i--) {
+            if (digits[i] + next == 10) {
+                digits[i] = 0;
+                next = 1;
+            }
+            else {
+                digits[i] += next;
+                next = 0;
+                break;
+            }
+        }
+        vector<int> res;
+        if (next)
+            res.push_back(1);
+        for (int i =0; i < digits.size(); i++)
+            res.push_back(digits[i]);
+        return res;
+    }
+};
