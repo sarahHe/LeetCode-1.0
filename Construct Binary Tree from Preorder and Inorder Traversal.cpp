9f7e19bc1547bab96ce9e57058a15daefd4143aa
sarahHe <hesha@umich.edu>
2015-03-04 20:37:19 -0500

    Update Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -13,20 +13,25 @@
  */
 class Solution {
 public:
+    //p is the index of the root in preorder
     TreeNode *helper(vector<int> &preorder, vector<int> &inorder, int p, int start, int end) {
         //if care about the coding style, could create a struct, which contains p, start and end.
         if (p == preorder.size() || end < 0 || start > end) return NULL;
             
         TreeNode *root = new TreeNode(preorder[p]);
-        if (start == end)   return root;
+        if (start == end)   return root; // important
         int i = start;
+        
+        //find the position of root in inorder
         while (i <= end) {
             if (preorder[p] == inorder[i])
                 break;
             i++;
         }
         int left_len = i - start;
+        //every node in the left tree is to the left of the root(i), the same with right
         root->left = helper(preorder, inorder, p + 1, start, i - 1);
+        //the root for the right part is 1st node in the right part
         root->right = helper(preorder, inorder, p + left_len + 1, i + 1, end);
         return root;
     }
