d8c69d4003503f8a613c45a726cbf1021e3df46d
sarahHe <hesha@umich.edu>
2015-03-10 12:25:44 -0400

    Update and rename Sort Colors to Sort Colors.cpp

---
+++ Sort Colors.cpp
@@ -0,0 +1,17 @@
+// Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+// Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+        int red = 0, blue = n -1, i = 0;
+        while (i < n) {
+            if (A[i] == 0 && i > red) // i > red is a must
+                swap(A[red++], A[i]);
+            else if (A[i] == 2 && i < blue) // i < blue is a must
+                swap(A[blue--], A[i]);
+            else
+                i++;
+        }
+    }
+};
