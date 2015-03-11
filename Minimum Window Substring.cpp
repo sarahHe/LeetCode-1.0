4ebcb9dca7dff83c0cdb5162c7e2fffe7a27c6f2
sarahHe <hesha@umich.edu>
2015-03-10 23:31:37 -0400

    Update Minimum Window Substring.cpp

--- Minimum Window Substring.cpp
+++ Minimum Window Substring.cpp
@@ -1,33 +1,50 @@
+// Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+// For example,
+// S = "ADOBECODEBANC"
+// T = "ABC"
+// Minimum window is "BANC".
+
+// Note:
+// If there is no such window in S that covers all characters in T, return the emtpy string "".
+
+// If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
 class Solution {
 public:
     string minWindow(string S, string T) {
-        if (S.length() < T.length())
-            return "";
+        if (S.length() < T.length())    return "";
         
-        unordered_map<char, int> expect, found;
-        for (auto i : T)
-            expect[i]++;
-        int start = 0, count = 0, resStart = 0, len = INT_MAX;
+        int expected[256] = {0}, found[256] = {0},
+            count = 0, k = 0, start = 0,
+            miniLen = INT_MAX;
+        for (int i = 0; i < T.length(); i++) {
+            expected[T[i]]++;
+        }
+        
         for (int i = 0; i < S.length(); i++) {
-            if (expect[S[i]] == 0)
-                continue;
+            if (expected[S[i]] == 0) 
+                continue; 
                 
             found[S[i]]++;
-            if (found[S[i]] <= expect[S[i]])
+            if (found[S[i]] <= expected[S[i]]) // ++ is before this, so need =
                 count++;
-            if (count == T.length()) {
-                while (expect[S[start]] == 0 || found[S[start]] > expect[S[start]]) {
-                    found[S[start]]--;
-                    start++;
+    
+            if (count == T.length()) { 
+                while (expected[S[k]] == 0 || found[S[k]] > expected[S[k]]) {
+                    // expected[S[k] is to ignore unexpected ones; 
+                    // found[S[i]] > expected[S[i]] is to ignore the repeated char
+                    if (found[S[k]] > expected[S[k]]) {
+                        found[S[k]]--;
+                    }
+                    k++;
                 }
-                if (i - start + 1 < len) {
-                    len = i - start + 1;
-                    resStart = start;
+                if (i - k + 1 < miniLen) {
+                    miniLen = i - k + 1;
+                    start = k;
                 }
-            }
+            } // no need to decrease count
         }
-        if (len == INT_MAX)
-            return "";
-        return S.substr(resStart, len);
+        if (miniLen == INT_MAX) return "";
+        return S.substr(start, miniLen);
     }
 };
