4e5a72b0dbcd75c90ccfc7b9274c6d14f167da90
sarahHe <hesha@umich.edu>
2015-03-10 23:49:59 -0400

    Update Combinations.cpp

--- Combinations.cpp
+++ Combinations.cpp
@@ -1,22 +1,35 @@
+// Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+// For example,
+// If n = 4 and k = 2, a solution is:
+
+// [
+//   [2,4],
+//   [3,4],
+//   [2,3],
+//   [1,2],
+//   [1,3],
+//   [1,4],
+// ]
 class Solution {
 public:
-    void helper(vector<vector<int> > &res, vector<int> &tmp, int n, int j, int k) {
-        if (!k) res.push_back(tmp);
+    void combine_helper(vector<vector<int> > &res, vector<int> tmp, int pos, int n, int k) {
+        if (tmp.size() == k) {
+            res.push_back(tmp);
+            return;
+        }
         
-        for (int i = j; i <= n; i++) {
-            // if (tmp.find(i) != tmp.end())   continue;
+        for (int i = pos; i<= n; i++) {
             tmp.push_back(i);
-            helper(res, tmp, n, i + 1, k - 1);
-            tmp.pop_back();
+            combine_helper(res, tmp, i + 1, n , k);
+            tmp.pop_back(); // important
         }
     }
 
     vector<vector<int> > combine(int n, int k) {
         vector<vector<int> > res;
-        if (!n || n < k || n < 1 || k < 1)    return res;
-        
         vector<int> tmp;
-        helper(res, tmp, n, 1, k);
+        combine_helper(res, tmp, 1, n, k);
         return res;
     }
 };
