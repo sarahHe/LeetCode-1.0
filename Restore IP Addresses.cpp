5454f0a4b0cd43a94d82ac4759af50e3cdc4b73b
sarahHe <hesha@umich.edu>
2015-03-12 00:05:17 -0400

    Update Restore IP Addresses.cpp

--- Restore IP Addresses.cpp
+++ Restore IP Addresses.cpp
@@ -48,3 +48,38 @@ public:
         return res;
     }
 };
+
+
+2015.3.11 update // although it's not as clean as the previous one
+bool isValid(string s) {
+    if (s.length() == 1)
+        return s >= "0" && s <= "9";
+    if (s.length() == 2)
+        return s >= "10" && s <= "99";
+    if (s.length() == 3)
+        return s >= "100" && s <= "255";
+    return false;
+}
+
+void restore_helper(vector<string> &res, string tmp, string s, int pos, int count) {
+    if (count == 4) {
+        if (pos == s.length())
+            res.push_back(tmp.substr(1));
+        return;
+    }
+    
+    for (int i = 1; i <= 3; i++) {
+        if (pos + i > s.length() || !isValid(s.substr(pos, i)))
+            return;
+    
+        restore_helper(res, tmp + "." + s.substr(pos, i), s, i + pos, count + 1);
+    }
+}
+
+vector<string> restoreIpAddresses(string s) {
+    vector<string>  res;
+    if (s.length() < 4 || s.length() > 12)
+        return res;
+    restore_helper(res, "", s, 0, 0);
+    return res;
+}
