d3e6954e4aa0985a1fbedbc425ed4fbebac2c090
sarahHe <hesha@umich.edu>
2015-04-18 22:28:29 -0400

    2015.4.18

--- Dungeon Game.cpp
+++ Dungeon Game.cpp
@@ -20,3 +20,41 @@ public:
         return dp[0][0]+1;
     }
 };
+
+
+
+2015.4.18 update
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int> > &dungeon) {
+        if (dungeon.empty() || dungeon[0].empty())  return 0;
+        
+        vector<vector<int> > D(dungeon.size() + 1, vector<int> (dungeon[0].size() + 1, INT_MAX));
+        D[D.size() - 1][D[0].size() - 2] = 1;
+        D[D.size() - 2].back() = 1;
+        for (int i = dungeon.size() - 1; i >= 0; i--) {
+            for (int j = dungeon[0].size() - 1; j >= 0; j--) {
+                int min_safe_exit = min(D[i+1][j], D[i][j+1]);
+                D[i][j] = max(min_safe_exit - dungeon[i][j], 1);
+            }
+        }
+        return D[0][0];
+    }
+};
+
+// we can use the original dungeno for DP data storage. Use O(mn) time, O(1) memory.
+class Solution {
+    public:
+      int calculateMinimumHP(vector<vector<int> > &dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        dungeon[m - 1][n - 1] = dungeon[m - 1][n - 1] > 0 ? 1 : abs(dungeon[m - 1][n - 1]) + 1;
+        for (int i = n-2; i >= 0; --i)
+          dungeon[m - 1][i] = max(dungeon[m - 1][i + 1] - dungeon[m - 1][i],1);
+        for (int i = m - 2; i >= 0; --i)
+          dungeon[i][n - 1] = max(dungeon[i + 1][n - 1] - dungeon[i][n - 1],1);
+        for (int i = m-2; i >= 0;--i)
+          for (int j = n-2; j >= 0; --j)
+            dungeon[i][j] = max(min(dungeon[i + 1][j], dungeon[i][j + 1]) - dungeon[i][j],1);
+        return dungeon[0][0];
+      }
+ };
