91189e7f1d932dc90a96eef86546468f7a1dcc39
sarahHe <hesha@umich.edu>
2015-04-20 01:28:33 -0400

    2015.4.19

--- Intersection of Two Linked Lists.cpp
+++ Intersection of Two Linked Lists.cpp
@@ -49,3 +49,37 @@ public:
             return NULL;
     }
 };
+
+2015.4.19 update
+// Two pointer solution (O(n+m) running time, O(1) memory):
+// Maintain two pointers pA and pB initialized at the head of A and B, respectively. Then let them both traverse through the lists, one node at a time.
+// When pA reaches the end of a list, then redirect it to the head of B (yes, B, that's right.); similarly when pB reaches the end of a list, redirect it the head of A.
+// If at any point pA meets pB, then pA/pB is the intersection node.
+// To see why the above trick would work, consider the following two lists: A = {1,3,5,7,9,11} and B = {2,4,9,11}, which are intersected at node '9'. Since B.length (=4) < A.length (=6), pB would reach the end of the merged list first, because pB traverses exactly 2 nodes less than pA does. By redirecting pB to head A, and pA to head B, we now ask pB to travel exactly 2 more nodes than pA would. So in the second iteration, they are guaranteed to reach the intersection node at the same time.
+// If two lists have intersection, then their last nodes must be the same one. So when pA/pB reaches the end of a list, record the last element of A/B respectively. If the two last elements are not the same one, then the two lists have no intersections.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (headA == NULL || headB == NULL) return NULL;
+        
+        ListNode *pa = headA, *pb = headB;
+        while (pa != pb) {
+            // if pa == NULL or Pb == NULL, no intersection
+            pa = pa->next;
+            pb = pb->next;
+            
+            if (pa == pb)   return pa;
+            if (pa == NULL) pa = headB;
+            if (pb == NULL) pb = headA;
+        }
+        return pa;
+    }
+};
