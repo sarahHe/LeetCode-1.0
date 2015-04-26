ff5f5b8fd12eda00233862f89526c9d10589dbbe
sarahHe <hesha@umich.edu>
2015-04-26 18:35:46 -0400

    Update Palindrome_Partitioning.cpp

--- Palindrome_Partitioning.cpp
+++ Palindrome_Partitioning.cpp
@@ -1,33 +1,46 @@
+// Given a string s, partition s such that every substring of the partition is a palindrome.
+
+// Return all possible palindrome partitioning of s.
+
+// For example, given s = "aab",
+// Return
+
+//   [
+//     ["aa","b"],
+//     ["a","a","b"]
+//   ]
 class Solution {
 public:
-    bool isPld(string s) {
-        for (int i = 0; i< s.length()/2; i++) {
-            if (s[i] != s[s.length() - i - 1])
+    bool isPalindrome(string &s, int start, int end) {
+        while (start < end) {
+            if (s[start++] != s[end--])
                 return false;
         }
         return true;
     }
-    
-    bool helper(string s, vector<string> &tmp, vector<vector<string> > &res) {
-        if (s == "")    return true;
-        for (int i = 0; i < s.length(); i++) {
-            for (int len = 1; len <= s.length()- i; len++) {
-                if (isPld(s.substr(0, len))) {
-                    tmp.push_back(s.substr(0, len));
-                    if (helper(s.substr(len), tmp, res))
-                        res.push_back(tmp);
-                    tmp.pop_back();
-                }
-            }
-            return false;
+
+    void dfs(vector<vector<string>> &res, vector<string> &tmp, int pos, string &s) {
+        if (pos == s.length()){
+            res.push_back(tmp);
+            return; 
+        }  
+        
+        for (int i = pos; i < s.length(); ++i) {
+            if (!isPalindrome(s, pos, i)) 
+                continue;
+                
+            tmp.push_back(s.substr(pos, i - pos + 1));
+            dfs(res, tmp, i+1, s); // i+1 rather than pos+1
+            tmp.pop_back();
         }
-        return false;
     }
-    
+
     vector<vector<string>> partition(string s) {
+        vector<vector<string> > res;
+        if (s == "")    return res;
+        
         vector<string> tmp;
-        vector<vector<string>> res;
-        helper(s, tmp, res);
+        dfs(res, tmp, 0, s);
         return res;
     }
 };
