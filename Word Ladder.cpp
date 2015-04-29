d67eaaaf24760fa0d41a02f7ac012f5035ebb5d9
sarahHe <hesha@umich.edu>
2015-04-29 11:27:46 -0400

    Update Word Ladder.cpp

--- Word Ladder.cpp
+++ Word Ladder.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int ladderLength(string start, string end, unordered_set<string> &dict) {
-        if (dict.empty())   return 0;
-    
-        queue<string> matched;
-        matched.push(start);
-        unordered_map<string, int>   step_map;
-        step_map[start] = 1;
-    
-        while (!matched.empty()) {
-            string str = matched.front();
-            matched.pop();
-            for (int i = 0; i < str.length(); i++) {
-                string copy_str = str;
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
+        if (wordDict.empty())   return 0;
+        
+        queue<string> q;
+        unordered_map<string, int> step_map;
+        step_map[beginWord] = 1;
+        q.push(beginWord);
+        
+        while (!q.empty()) {
+            string cur = q.front();
+            q.pop();
+            for (int i = 0; i < cur.length(); i++) {
+                string copy_cur = cur; //!!
                 for (char j = 'a'; j <= 'z'; j++) {
-                    if (copy_str[i] == j)    continue;
+                    if (j == cur[i])    continue; //!!
                     
-                    copy_str.replace(i, 1, 1, j);
-                    if (copy_str == end)
-                        return step_map[str] + 1;
+                    copy_cur.replace(i, 1, 1, j); //replace(pos, len, times, new_str), change the string
+                    if (copy_cur == endWord)
+                        return step_map[cur] + 1;
                     
-                    if (dict.find(copy_str) != dict.end() && !step_map[copy_str]) {
-                        matched.push(copy_str);
-                        step_map[copy_str] = step_map[str] + 1;
+                    if (wordDict.find(copy_cur) != wordDict.end() && !step_map[copy_cur]) {
+                        //if in the dict and not in step_map(to get smallest steps)
+                        step_map[copy_cur] = step_map[cur] + 1;
+                        q.push(copy_cur);
                     }
-                    
                 }
             }
         }
