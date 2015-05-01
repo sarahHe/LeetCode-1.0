7c611a10c8baea1c563acbdff196054fc936cdb7
sarahHe <hesha@umich.edu>
2015-05-01 01:12:47 -0400

    Update and rename Binary Tree Maximum Path Sum to Binary Tree Maximum Path Sum.cpp

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -30,3 +30,24 @@ public:
          return global;
     }
 };
+
+
+2015.4.30 update
+//more clear
+class Solution {
+public:
+    int findPath(TreeNode *root, int &res) {
+        if (!root)  return 0;
+        int left = findPath(root->left, res),
+            right = findPath(root->right, res);
+        res = max(res, left + right + root->val);
+        return max(0, max(left, right) + root->val);
+    }
+
+    int maxPathSum(TreeNode *root) {
+        if (!root)  return 0;
+        int res = root->val;
+        findPath(root, res);
+        return res;
+    }
+};
