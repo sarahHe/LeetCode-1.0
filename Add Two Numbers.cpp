9ccb367334f7e299b1d087bc0462c51a42d53f1f
sarahHe <hesha@umich.edu>
2015-05-05 12:09:38 -0400

    Update and rename Add Two Numbers to Add Two Numbers.cpp

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -40,3 +40,40 @@ public:
         return res->next;
     }
 };
+
+
+
+2015.5.5 update
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(-1), *cur = &dummy;
+        int sum = 0;
+        while (l1 || l2) {
+            sum /= 10;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            ListNode *node = new ListNode(sum % 10);
+            cur->next = node;
+            cur = cur->next;
+        }
+        if (sum / 10)
+            cur->next = new ListNode(sum/10);
+        return dummy.next;
+        
+    }
+};
