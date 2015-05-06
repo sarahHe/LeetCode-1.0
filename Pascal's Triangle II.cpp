bfa00e7bcf200953c913afffee7d055aa881af12
sarahHe <hesha@umich.edu>
2015-05-05 21:21:13 -0400

    Update Pascal's Triangle II.cpp

--- Pascal's Triangle II.cpp
+++ Pascal's Triangle II.cpp
@@ -19,3 +19,18 @@ public:
         return dp;
     }
 };
+
+
+2015.5.5 update
+//reduce time from 6ms to 3ms
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex+1, 0);
+        res[0] = 1;
+        for (int i = 0; i <= rowIndex; i++) 
+            for (int j = i; j > 0; j--)
+                res[j] += res[j-1];
+        return res;
+    }
+};
