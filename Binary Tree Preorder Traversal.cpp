03155a6d707c1c5407596634c09e51a3c31f5083
sarahHe <hesha@umich.edu>
2015-05-16 14:44:20 -0700

    Update and rename Binary Tree Preorder Traversal to Binary Tree Preorder Traversal.cpp

--- Binary Tree Preorder Traversal.cpp
+++ Binary Tree Preorder Traversal.cpp
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -9,19 +9,20 @@
  */
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
-        if (root == NULL)   return res;
-
+        if (root == NULL)
+            return res;
+            
         stack<TreeNode*> s;
         s.push(root);
         while (!s.empty()) {
             TreeNode *cur = s.top();
             s.pop();
-            res.push_back(cur->val);
-            if (cur->right)
+            res.push_back(root->val);
+            if (cur->right) //因为是stack所以先右后左
                 s.push(cur->right);
-            if (cur->left)
+            if (cur->left) 
                 s.push(cur->left);
         }
         return res;
