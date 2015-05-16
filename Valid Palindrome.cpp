afacabab903e62f235ad13653940476fb2663aa5
sarahHe <hesha@umich.edu>
2015-05-16 09:53:30 -0700

    Update and rename Valid Palindrome to Valid Palindrome.cpp

--- Valid Palindrome.cpp
+++ Valid Palindrome.cpp
@@ -23,3 +23,22 @@ public:
         return true;
     }
 };
+
+
+2015.5.16 update
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s == "")    return true;
+        
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !isalnum(s[i]))  i++;
+            while (i < j && !isalnum(s[j]))  j--;
+            
+            if (i < j && tolower(s[i]) != tolower(s[j]))   return false;
+            i++; j--;
+        }
+        return true;
+    }
+};
