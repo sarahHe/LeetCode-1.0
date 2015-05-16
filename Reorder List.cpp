4443e3a5a129408f1f803e923c68a101c45bace3
sarahHe <hesha@umich.edu>
2015-05-16 16:32:21 -0700

    Update Reorder List.cpp

--- Reorder List.cpp
+++ Reorder List.cpp
@@ -37,3 +37,53 @@ public:
         cur->next = NULL;
     }
 };
+
+
+2015.5.16 update
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == NULL || head->next == NULL)   return;
+        
+        //find mid
+        ListNode *fast = head, *slow = head;
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        ListNode *head2 = slow->next;
+        slow->next = NULL; //not slow = NULL;
+        
+        //reverse
+        ListNode *p = head2;
+        while(p->next) { //not while(p), and p is always the first node before reverse, so p won't change
+            ListNode *q = p->next;
+            p->next = q->next;
+            q->next = head2; // not q->next = p
+            head2 = q;
+        }
+        
+        //merge
+        ListNode *cur = new ListNode(-1);
+        while (head || head2) {
+            if (head) {
+                cur->next = head;
+                head = head->next;
+                cur = cur->next;
+            }
+            if (head2) {
+                cur->next = head2;
+                head2 = head2->next;
+                cur = cur->next;
+            }
+        }
+    }
+};
