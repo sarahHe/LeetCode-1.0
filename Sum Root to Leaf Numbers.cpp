961478c3db5d227572ae246716b19a5a914ca794
sarahHe <hesha@umich.edu>
2015-05-22 21:31:38 -0700

    Update Sum Root to Leaf Numbers.cpp

--- Sum Root to Leaf Numbers.cpp
+++ Sum Root to Leaf Numbers.cpp
@@ -43,3 +43,27 @@ public:
         return getSum(root, 0);
     }
 };
+
+
+
+2015.5.22 update from 5ms to 4ms
+ //居然做出了最优解。
+class Solution {
+public:
+    int getNum(TreeNode* root, int res) {
+        if (!root->left && !root->right)  return res; //对于涉及左右节点分别计算的，要把左右都没有节点的情况作为base case
+        
+        int sum = 0;
+        if (root->left)
+            sum += getNum(root->left, res * 10 + root->left->val);
+        if (root->right)
+            sum += getNum(root->right, res * 10 + root->right->val);
+        return sum;
+    }
+
+    int sumNumbers(TreeNode* root) {
+        if (!root)  return 0;
+        
+        return getNum(root, root->val);
+    }
+};
