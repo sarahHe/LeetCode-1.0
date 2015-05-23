c6d39c8f3b58a93c2c4946b0b8f915f1d014dc04
sarahHe <hesha@umich.edu>
2015-05-22 22:37:29 -0700

    Update Letter Combinations of a Phone Number.cpp

--- Letter Combinations of a Phone Number.cpp
+++ Letter Combinations of a Phone Number.cpp
@@ -1,4 +1,3 @@
-//classical DP
 class Solution {
 public:
     void generate(string digits, vector<string> trans, int pos, string tmp, vector<string> &res) {
@@ -22,4 +21,32 @@ public:
         generate(digits, trans, 0,"", res);
         return res;
     }
+};
+
+
+2015.5.22 update
+class Solution {
+public:
+    void combine(string &digits, int pos, vector<string> &res, string tmp) {
+        if (tmp.length() == digits.length())
+            res.push_back(tmp);
+            
+        for (int i = pos; i < digits.length(); i++) {
+            for (auto j : letter[digits[i] - '0']) {
+                tmp += j;
+                combine(digits, i + 1, res, tmp);
+                tmp.resize(tmp.length() - 1);
+            }
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits == "")   return vector<string>();
+        
+        vector<string> res;
+        combine(digits, 0, res, "");
+        return res;
+    }
+private:
+    vector<string> letter = {"--", "--", "abc", "edf", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; // make use of private data could speed up a little bit
 };
