2b1fb3489582759876a76364d04ba199d1d87ab2
sarahHe <hesha@umich.edu>
2015-05-24 11:16:32 -0700

    Update Longest Common Prefix.cpp

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -4,20 +4,17 @@
 //try to reduce the time of comparing
 class Solution {
 public:
-    string longestCommonPrefix(vector<string> &strs) {
-        if (strs.empty())   return "";
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty() || strs[0] == "")  return ""; // edge case ["", ""]
         
-        int length = strs[0].length();
-        for (int i = 0; i < (int)strs.size(); i++) {
-            if (strs[i] == "")  return "";
-            
-            int j = 0;
-            for (; j < (int)strs[i].length(); j++) {
-                    if (j >= length || strs[i][j] != strs[0][j]) // j>= length
-                        break;
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 0; j < strs.size(); j++) {
+                if (strs[0][i] != strs[j][i]) {
+                    return strs[0].substr(0, i);
+                }
             }
-            length = j;
         }
-        return strs[0].substr(0, length);
+        return strs[0];
     }
+    
 };
