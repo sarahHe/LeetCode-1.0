b8548a2ef149f853dbd88815d641a883521bef79
sarahHe <hesha@umich.edu>
2015-05-23 18:37:55 -0700

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -65,7 +65,7 @@ public:
             return 0;
         for (int i = triangle.size() - 2; i >= 0; i--) {
             for (int j = 0; j < triangle[i].size(); j++) {
-                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);//避免最左最右边界的讨论
             }
         }
         return triangle[0][0];
