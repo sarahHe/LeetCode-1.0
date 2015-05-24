a0ec21e908dacf60d2544108b7e85d6d4182f474
sarahHe <hesha@umich.edu>
2015-05-24 11:43:34 -0700

    Update Longest Common Prefix.cpp

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -7,6 +7,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty() || strs[0] == "")  return ""; // edge case ["", ""]
         
+        //sort(strs.begin(), strs.end()); //it's also good to sort first
         for (int i = 0; i < strs[0].length(); i++) {
             for (int j = 0; j < strs.size(); j++) {
                 if (strs[0][i] != strs[j][i]) {
