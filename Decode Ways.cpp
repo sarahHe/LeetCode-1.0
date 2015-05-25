2ad7a898a9a55b3dd5bf28146787dd03c31cb7d3
sarahHe <hesha@umich.edu>
2015-05-25 16:14:37 -0700

    Update Decode Ways.cpp

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -12,13 +12,12 @@ class Solution {
 public:
     int numDecodings(string s) {
         if (s == "")    return 0;
-        
-        vector<int> dp(s.length()+1, 0);
+        vector<int> dp(s.length()+1, 0); // s.length() + 1
         dp[0] = 1;
-        for (int i = 1; i <= s.length(); i++) {
-            if (s[i-1] != '0')
-                dp[i] = dp[i-1];
-            if (i > 1 && s.substr(i-2, 2) >= "10" && s.substr(i-2, 2) <= "26")
+        for (int i = 1; i <= s.length(); i++) { //<=
+            if (s[i - 1] != '0')
+                dp[i] = dp[i - 1];
+            if (i > 1 && (s.substr(i-2, 2) >= "10" && s.substr(i-2, 2) <= "26"))
                 dp[i] += dp[i-2];
         }
         return dp.back();
