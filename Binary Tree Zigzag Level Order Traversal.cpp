c29fc020a3f8d0cc4d86260300b4a8ed83ce5e2b
sarahHe <hesha@umich.edu>
2015-05-25 14:24:57 -0700

    Update Binary Tree Zigzag Level Order Traversal.cpp

--- Binary Tree Zigzag Level Order Traversal.cpp
+++ Binary Tree Zigzag Level Order Traversal.cpp
@@ -42,3 +42,34 @@ public:
         return res;
     }
 };
+
+
+
+
+2015.5.25 update
+//recursive soltion
+class Solution {
+public:
+    void travel(TreeNode* cur, vector<vector<int>> &res, int level) {
+        if (cur == NULL)    return;
+        
+        if (res.size() <= level) {
+            vector<int> newLevel;
+            res.push_back(newLevel);
+        }
+        
+        if (level % 2 == 0)
+            res[level].push_back(cur->val);
+        else
+            res[level].insert(res[level].begin(), cur->val);
+        
+        travel(cur->left, res, level+1);
+        travel(cur->right, res, level+1);
+    }
+
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        travel(root, res, 0);
+        return res;
+    }
+};
