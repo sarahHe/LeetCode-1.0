ba627f634225806a99f4ae6b4ef6114fa3e799b7
sarahHe <hesha@umich.edu>
2015-05-25 23:07:28 -0700

    Update and rename Reverse Integer to Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -12,3 +12,13 @@ public:
         return res;
     }
 };
+
+2015.5.25 update
+int reverse(int x) {
+    long long res = 0; // must change it to long long!!
+    while (x) {
+        res = res * 10 + x % 10;
+        x /= 10;
+    }
+    return (res < INT_MIN || res > INT_MAX) ? 0 : res;
+}
