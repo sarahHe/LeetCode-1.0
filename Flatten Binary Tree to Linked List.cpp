3755d69009869464381ccc82c07958457abd7cc8
sarahHe <hesha@umich.edu>
2015-05-27 16:42:09 -0700

    Update Flatten Binary Tree to Linked List.cpp

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -24,3 +24,26 @@ public:
         root->right = pRight;
     }
 };
+
+
+
+2015.5.27 update:
+ //morris 
+ //largest node in the left tree -> child = direct right child
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode *cur = root;
+        while (cur) {
+            if (cur->left) {
+                TreeNode *pre = cur->left;
+                while (pre->right)
+                    pre = pre->right;
+                pre->right = cur->right;
+                cur->right = cur->left;
+                cur->left = NULL;
+            }
+            cur = cur->right;
+        }
+    }
+};
