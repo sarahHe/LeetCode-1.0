0968c92e8a80fe71beff045c5b0995907ad0562b
sarahHe <hesha@umich.edu>
2015-05-28 22:43:13 -0700

    Update Evaluate Reverse Polish Notation.cpp

--- Evaluate Reverse Polish Notation.cpp
+++ Evaluate Reverse Polish Notation.cpp
@@ -1,69 +1,28 @@
-2015.4.20 update
-//run time reduced from 56ms to 18ms
-int evalRPN(vector<string>& tokens) {
-    stack<int> numbers;
-    for (auto i : tokens) {
-        int n1, n2;
-        if (i == "+") {
-                n1 = numbers.top();
-                numbers.pop();
-                n2 = numbers.top();
-                numbers.pop();
-                numbers.push(n1 + n2);
+2015.5.28 update
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                int o2 = s.top();
+                s.pop();
+                int o1 = s.top();
+                s.pop();
+                if (tokens[i] == "+")
+                    s.push(o1 + o2);
+                else if (tokens[i] == "-")
+                    s.push(o1 - o2);
+                else if (tokens[i] == "*")
+                    s.push(o1 * o2);
+                else
+                    s.push(o1 / o2);
+            } else 
+                s.push(stoi(tokens[i]));
         }
-        else if (i == "-") {
-                n1 = numbers.top();
-                numbers.pop();
-                n2 = numbers.top();
-                numbers.pop();
-                numbers.push(n2 - n1);
-        }
-        else if (i == "*") {
-                n1 = numbers.top();
-                numbers.pop();
-                n2 = numbers.top();
-                numbers.pop();
-                numbers.push(n1 * n2);
-        }
-        else if (i == "/") {
-                n1 = numbers.top();
-                numbers.pop();
-                n2 = numbers.top();
-                numbers.pop();
-                numbers.push(n2 / n1);
-        }
-        else
-            numbers.push(stoi(i));
+        return s.top();
     }
-    return numbers.top();
-}
-
-
-//Found a cool recursive way. A bit slow
-int eval_expression(vector<string>& tokens, int& pt)
-{
-    string s = tokens[pt];
-
-    if(s == "+" || s == "-" || s == "*" || s== "/") // tokens[r] is an operator
-    {
-        pt--;
-        int v2 = eval_expression(tokens, pt);
-        pt--;
-        int v1 = eval_expression(tokens, pt);
-        if(s == "+")
-            return v1 + v2;
-        else if(s == "-")
-            return v1 - v2;
-        else if(s == "*")
-            return v1 * v2;
-        else 
-            return v1 / v2;
-    }
-    else // tokens[r] is a number
-    {
-        return atoi(s.c_str());
-    }
-}
+};
 
 int evalRPN(vector<string> &tokens) {
     int pt = tokens.size()-1; 
