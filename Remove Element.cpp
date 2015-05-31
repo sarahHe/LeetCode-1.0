f682bda3cbd67cdcd0bd5014375bf85163d756d3
sarahHe <hesha@umich.edu>
2015-05-31 00:03:28 -0700

    Create Remove Element.cpp

---
+++ Remove Element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) continue;
+            nums[pos++] = nums[i];
+        }
+        return pos;
+    }
+};
