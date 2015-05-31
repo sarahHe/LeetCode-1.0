41fb24e0023234a397eedf114a4aeabd187f76ec
sarahHe <hesha@umich.edu>
2015-05-31 14:47:50 -0700

    Update Find Minimum in Rotated Sorted Array.cpp

--- Find Minimum in Rotated Sorted Array.cpp
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -3,9 +3,9 @@
 // Looking at subarray with index [start,end]. We can find out that if the first member is less than the last member, 
 //there's no rotation in the array. So we could directly return the first element in this subarray.
 
-// If the first element is larger than the last one, then we compute the element in the middle, 
-//and compare it with the first element. If value of the element in the middle is larger than the first element, 
-//we know the rotation is at the second half of this array. Else, it is in the first half in the array.
+//只要有rotate 最左 > 最右
+//中间的 》 最左 =》rotate部分在右边
+//否则 rotate在左边
 class Solution {
 public:
     int findMin(vector<int> &num) {
