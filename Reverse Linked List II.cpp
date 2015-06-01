0e074ad3b229edeb92162129358c15429cf957af
sarahHe <hesha@umich.edu>
2015-05-31 22:10:05 -0700

    Update Reverse Linked List II.cpp

--- Reverse Linked List II.cpp
+++ Reverse Linked List II.cpp
@@ -6,34 +6,26 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+2015.5.31 update
 class Solution {
 public:
-    ListNode *reverseBetween(ListNode *head, int m, int n) {
-        if (!head || !head->next || m >= n)   return head;
-        
-        ListNode *pre = head, *cur = head;
-        int i = 1;
-        while (i < m && cur) {
-            pre = cur;
-            cur = cur->next;
-            i++;
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode *pre = new ListNode(-1), *q = pre;
+        pre->next = head;
+        int pos = 1;
+        while (pos != m) {
+            pre = pre->next;
+            head = head->next;
+            pos++;
         }
-        if (!cur)   return head;
         
-        ListNode *p = cur->next;
-        while (i < n && p) {
-            cur->next = p->next;
-            if (m == 1) {
-                p->next = head;
-                head = p;
-            }
-            else {
-                p->next = pre->next;
-                pre->next = p;
-            }
-            p = cur->next;
-            i++;
+        while (pos != n && head) {
+            ListNode *t = head->next;
+            head->next = t->next;
+            t->next = pre->next;
+            pre->next = t;
+            pos++;
         }
-        return head;
+        return q->next;
     }
 };
