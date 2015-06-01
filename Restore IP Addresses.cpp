b0380eb8080014dd6e75a6c0123ff442b9bbff51
sarahHe <hesha@umich.edu>
2015-05-31 23:05:37 -0700

    Update Restore IP Addresses.cpp

--- Restore IP Addresses.cpp
+++ Restore IP Addresses.cpp
@@ -8,78 +8,38 @@
 
 // ip address should be [0,255]
 //be careful about the precondition before doing anything
+2015.5.31 update
 class Solution {
 public:
-    bool isValid(string num) {
-        if (num.length() == 1)
-            return num >= "0" && num <= "9";
-        else if (num.length() == 2)
-            return num >= "10" && num <= "99";
-        else if (num.length() == 3)
-            return num >= "100" && num <= "255";
-        else
-            return false;
+    bool isValid(string sub) {
+        if (sub.length() == 1)
+            return sub >= "0" && sub <= "9"; // for case like 01
+        else if (sub.length() == 2)
+            return sub >= "10" && sub <= "99";
+        else 
+            return sub >= "100" && sub <= "255";
     }
-    
-    void helper(string s, vector<string> &res, string tmp, int k) {
-        if (k == 3) {
-            if (isValid(s)) {
-                tmp += '.'+s;
-                res.push_back(tmp.substr(1));
-            }
+
+    void restore(vector<string> &res, string tmp, string s, int pos, int count) {
+        if (count == 4 && pos < s.length()) return;
+        
+        if (count == 4 && pos == s.length()) {
+            res.push_back(tmp.substr(1));
             return;
         }
-        
-        for (int i=1; i<4; i++) {
-            if (s.length() >= i && isValid(s.substr(0,i))) {
-                helper(s.substr(i), res, tmp +'.'+ s.substr(0, i), k+1);
+    
+        for (int j = 1; j <= 3 && pos + j <= s.length(); j++) {
+            string sub = s.substr(pos, j);
+            if (isValid(sub)) {
+                restore(res, tmp + "." + sub, s, pos + j, count + 1);
             }
-            else
-                return;
         }
     }
-    
+
     vector<string> restoreIpAddresses(string s) {
-        vector<string>  res;
-        if (s.length() < 4 || s.length() > 12)
-            return res;
-        
-        helper(s, res, "", 0);
+        vector<string> res;
+        if (s == "")    return res;
+        restore(res, "", s, 0, 0);
         return res;
     }
 };
-
-
-2015.3.11 update // although it's not as clean as the previous one
-bool isValid(string s) {
-    if (s.length() == 1)
-        return s >= "0" && s <= "9";
-    if (s.length() == 2)
-        return s >= "10" && s <= "99";
-    if (s.length() == 3)
-        return s >= "100" && s <= "255";
-    return false;
-}
-
-void restore_helper(vector<string> &res, string tmp, string s, int pos, int count) {
-    if (count == 4) {
-        if (pos == s.length())
-            res.push_back(tmp.substr(1));
-        return;
-    }
-    
-    for (int i = 1; i <= 3; i++) {
-        if (pos + i > s.length() || !isValid(s.substr(pos, i)))
-            return;
-    
-        restore_helper(res, tmp + "." + s.substr(pos, i), s, i + pos, count + 1);
-    }
-}
-
-vector<string> restoreIpAddresses(string s) {
-    vector<string>  res;
-    if (s.length() < 4 || s.length() > 12)
-        return res;
-    restore_helper(res, "", s, 0, 0);
-    return res;
-}
