a0501e04977ac95e68fbbb6105bfbf197ba9649e
sarahHe <hesha@umich.edu>
2015-06-03 18:20:30 -0700

    Update Longest Valid Parentheses.cpp

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -26,3 +26,25 @@ public:
         return global;
     }
 };
+
+
+2015.6.3 update
+//dp
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        vector<int> dp(s.length(), 0);
+        int start = 0, maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(')    start++;
+            if (s[i] == ')' && start > 0) { // && start > 0
+                dp[i] = 2 + dp[i-1];
+                if (i - dp[i] > 0)
+                    dp[i] += dp[i - dp[i]];
+                start--; // start--
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+};
