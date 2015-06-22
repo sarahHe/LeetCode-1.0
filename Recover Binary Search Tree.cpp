18f161466660dd3fafa15abb53cbc998cd014972
sarahHe <hesha@umich.edu>
2015-06-21 21:58:04 -0700

    Update Recover Binary Search Tree.cpp

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -14,7 +14,7 @@
 // 有一个技巧是如果遍历整个序列过程中只出现了一次次序错误，
 // 说明就是这两个相邻节点需要被交换。如果出现了两次次序错误，那就需要交换这两个节点。
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -24,71 +24,24 @@
  */
 class Solution {
 public:
-    void recoverTree(TreeNode *root) {
+    void inOrderTraversal(TreeNode* root) {
         if (!root)  return;
-        
-        TreeNode *f1 = NULL, *f2 = NULL,
-            *pre, *cur = root, *parent = NULL;
-        while (cur) {
-            if (cur->left == NULL) {
-                if (parent && parent->val > cur->val) {
-                    if (f1 == NULL)
-                        f1 = parent;
-                    f2 = cur;
-                }
-                parent = cur;
-                cur = cur->right;
-            }
-            else { 
-                pre = cur->left;
-                while (pre->right && pre->right != cur)
-                    pre = pre->right;
-                    
-                if (pre->right == NULL) {
-                    pre->right = cur;
-                    cur = cur->left;
-                }
-                else {
-                    pre->right = NULL;
-                    if (parent->val > cur->val) {
-                        if (f1 == NULL)
-                            f1 = parent;
-                        f2 = cur;
-                    }
-                    parent = cur;
-                    cur = cur->right;
-                }
-            }
-        }
-        if (f1 && f2)
-            swap(f1->val, f2->val);
-    }
-};
-
-2015.3.11 update
-class Solution {
-public:
-    void inOrderTraversal(TreeNode *root) {
-        if (root == NULL)   return;
-        
         inOrderTraversal(root->left);
-        
         if (pre->val > root->val) {
             if (f1 == NULL)
                 f1 = pre;
-            
-            f2 = root; // no else
+            f2 = root; //no else for the neighboring nodes
         }
-        pre = root;
-        
+        pre = root;//!!
         inOrderTraversal(root->right);
     }
-    
-    void recoverTree(TreeNode *root) {
+
+    void recoverTree(TreeNode* root) {
         inOrderTraversal(root);
-        if (f1 && f2) // pay attention
-            swap(f1->val, f2->val);
+        if (f1 && f2)
+            swap(f1->val, f2->val);//swap value
     }
 private:
-    TreeNode *f1 = NULL, *f2 = NULL, *pre = new TreeNode(INT_MIN);
+    TreeNode *pre = new TreeNode(INT_MIN), *f1 = NULL, *f2 = NULL;
+    //must use private members, can't just use parameters. The value gets overrided in each stack
 };
