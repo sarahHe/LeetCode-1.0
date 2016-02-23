a7128af711864ab4b558095ebbc1e254e3ed8ab8
sarahHe <hesha@umich.edu>
2016-02-23 14:53:09 -0500

    Create Move Zeroes.cpp

---
+++ Move Zeroes.cpp
@@ -0,0 +1,16 @@
+//Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+//For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0)
+                nums[pos++] = nums[i];
+        }
+        while (pos < nums.size())
+            nums[pos++] = 0;
+    }
+};
