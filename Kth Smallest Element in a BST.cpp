a6cbd9e10dc5713eaf431377a0c998edb1380cc0
sarahHe <hesha@umich.edu>
2016-02-23 11:00:12 -0500

    Update Kth Smallest Element in a BST.cpp

--- Kth Smallest Element in a BST.cpp
+++ Kth Smallest Element in a BST.cpp
@@ -31,3 +31,29 @@ private:
             helper(root->right);
     }
 };
+
+ // DFS most preferable
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int count = nodeCount(root);
+        if (k <= count)
+            return kthSmallest(root->left, k);
+        else if (k > count + 1)
+            return kthSmallest(root->right, k - 1 - count); //1 is counted as current node
+        else
+            return root->val;
+    }
+    
+    int nodeCount(TreeNode *root) {
+        if (!root)
+            return 0;
+        return 1 + nodeCount(root->left) + nodeCount(root->right);
+    }
+};
+
+//What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? 
+//How would you optimize the kthSmallest routine?
+//Answer: add a new property as the number of subnodes
+
+
