517ce44c20212be5cd256359285d1d32b05bdf6a
sarahHe <hesha@umich.edu>
2016-02-23 22:37:01 -0500

    Update Palindrome Linked List.cpp

--- Palindrome Linked List.cpp
+++ Palindrome Linked List.cpp
@@ -1,3 +1,43 @@
 // 1. find the mid point of the list
 // 2. reverse the second half of linked list
 // 3. check the two half
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)   return true;
+        
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // reverse the second half
+        // slow->next is the head of second half
+        ListNode *h = slow->next, *q = h->next;
+        while (q) {
+            ListNode *t = slow->next;
+            slow->next = q;
+            h->next= q->next;
+            q->next = t;
+            q = h->next;
+        }
+        
+        // check if the two half the same
+        h = slow->next;
+        while (h) {
+            if (head->val != h->val)
+                return false;
+            head = head->next;
+            h = h->next;
+        }
+        return true;
+    }
+};
