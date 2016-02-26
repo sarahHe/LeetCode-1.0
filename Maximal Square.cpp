3039818e88de353bda11b722571e4b58266f1086
sarahHe <hesha@umich.edu>
2016-02-26 00:09:51 -0500

    Update Maximal Square.cpp

--- Maximal Square.cpp
+++ Maximal Square.cpp
@@ -36,3 +36,30 @@ public:
         return maxSize * maxSize;
     }
 };
+
+
+
+Optimaze solution: o(n) space
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty())    return 0;
+        
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int> dp(m+1, 0);
+        int pre = 0, maxSize = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int tmp = dp[j];
+                if (matrix[i][j-1] == '0')
+                    dp[j] = 0;
+                else {
+                    dp[j] = min(pre, min(dp[j], dp[j-1])) + 1;
+                    maxSize = max(maxSize, dp[j]);    
+                }
+                pre = tmp;
+            }
+        }
+        return maxSize * maxSize;
+    }
+};
