f2026695b80a8110b8b2a56c4e3a8dd7d1b43db6
sarahHe <hesha@umich.edu>
2016-02-26 13:33:18 -0500

    Update Regular Expression Matching.cpp

--- Regular Expression Matching.cpp
+++ Regular Expression Matching.cpp
@@ -43,3 +43,18 @@ public:
         return false;
     }
 };
+
+
+
+2016.2.26
+bool isMatch(string s, string p) {
+    if (p.empty())  return s.empty();
+    
+    if (p[1] == '*') {
+        return isMatch(s, p.substr(2)) ||
+                (!s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p) );
+    }
+    else {
+        return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1)); 
+    }
+}
