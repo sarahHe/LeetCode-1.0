ff5b28532ee6bdd1c36ef8a77bd0b24504e5bc09
sarahHe <hesha@umich.edu>
2016-02-27 16:51:35 -0500

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -12,3 +12,25 @@ public:
         return false;
     }
 };
+
+
+
+2016.2.27
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //binary search
+        int n = matrix.size(), m = matrix[0].size();
+        int left = 0, right = n * m - 1;
+        while (left < right) {
+            int mid = (left+right)/2;
+            if (matrix[mid/m][mid%m] == target)
+                return true;
+            if (matrix[mid/m][mid%m] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return matrix[left/m][left%m] == target;
+    }
+};
