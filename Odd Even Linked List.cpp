5c81a7c1a9140cba91f2192994f262b1bd380c97
sarahHe <hesha@umich.edu>
2016-02-26 23:41:51 -0500

    Create Odd Even Linked List.cpp

---
+++ Odd Even Linked List.cpp
@@ -0,0 +1,28 @@
+// Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+// You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next)   return head;
+        
+        ListNode *odd = head, *even = head->next, *evenHead = even;
+        while(even && even->next) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
